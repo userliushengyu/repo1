c6211313f22c47a7d3484a8355d8432dfa639460
liusehngyu <1079454045@qq.com>
2020-02-18 15:29:42 +0800

    将java工程添加到版本仓库

---
+++ helloworld/src/main/java/com/kuang/helloworld/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.kuang.helloworld.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+//  接口： http://localhost:8080/hello
+    @RequestMapping("/hello")
+    public String hello(){
+        //调用业务接收前端参数
+        return "hello,World";
+    }
+}
